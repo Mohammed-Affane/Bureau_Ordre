59fe74fce023d3c96cd2aac7ef979c2f00889458
AlijalalMin <alijalalprofe@gmail.com>
2025-06-30 16:09:52 +0200

    modifier les liens dans users folder

--- resources/views/dashboards/admin/users/index.blade.php
+++ resources/views/dashboards/admin/users/index.blade.php
@@ -26,7 +26,7 @@
                         </button>
                     </div>
                     <div class="mt-4">
-                        <p class="text-sm text-gray-500">Are you sure you want to delete this role? This action cannot be undone.</p>
+                        <p class="text-sm text-gray-500">Are you sure you want to delete this Utilisateur ? This action cannot be undone.</p>
                     </div>
                     <div class="mt-6 flex justify-end space-x-3">
                         <button type="button" onclick="closeModal()" class="px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -79,7 +79,7 @@
                                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $user->created_at }}</td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                         <div class="flex space-x-4">
-                                            <a href="{{ route('admin.roles.show', $user) }}" class="text-indigo-600 hover:text-indigo-900" title="View">
+                                            <a href="{{ route('admin.users.show', $user) }}" class="text-indigo-600 hover:text-indigo-900" title="View">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
